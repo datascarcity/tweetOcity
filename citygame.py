b19a12e94d1da101e6962fc821208c126fd2f6c2
urbanoVIII <rizzetto.m@gmail.com>
2016-11-13 17:28:08 +0100

    change cities database

--- citygame.py
+++ citygame.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import random
 import urllib
+from dbfread import DBF
 
 #AUTHENTICATION
 key = open('keys', 'r') #opens the file with the secret keys
@@ -16,53 +17,51 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+#LOADING THE TABLE FILE, IT ACTUALLY LOAD ALL THE FILE
+cities = DBF('ne_10m_populated_places_simple.dbf', load=True)
+
 #MAIN VARIABLES
 lat = 0 #Starting latitude and longitude
 lon = 0
 sleeping = 60 #Defines the waiting time between the tweets in seconds
-increase = 0.10 #Defines the coordinates increment
-iteration = 35 #Number of iterations counter
+iteration = 55 #Number of iterations counter
 zoom = 15 #Zoom Level
+numcities = len(cities)
 random.seed()
-
-#OPENING THE CITIES DATABASE
-with open('cities.txt') as f:
-    numcities =  sum(1 for _ in f)
-    f.close()
-    
-cities = open('cities.txt','r') #thanks to gael varoquaux for this file
-cities = cities.readlines()
 
 #MAIN LOOPS GENERATING THE IMAGE
 while True:
     api.update_status("Get ready! A new #quiz will be published in 10 minutes! Quiz nr. "+str(iteration))
+
     time.sleep(10)
     n = random.randrange(1,numcities,1)
-    print cities [n]
-    cityline = cities[n]
-    cityline = cityline.rstrip()
-    cityline = cityline.split('\t')
-    print cityline
-    lon = cityline[1]
-    lat = cityline[2]
+    print cities.records[n]['name']
+    print cities.records[n]['longitude']
+    print cities.records[n]['latitude']
+    lon = cities.records[n]['longitude']
+    lat = cities.records[n]['latitude']
     zoom = 15
+    
     test = "https://maps.googleapis.com/maps/api/staticmap?center="+str(lat)+","+str(lon)+"&zoom="+str(zoom)+"&size=640x640&maptype=satellite"
     urllib.urlretrieve(test, "image"+".png") #Saves the image overwriting the existing one
     time.sleep(5) #gives time to fetch the content and save it
     api.update_with_media('image.png',"Guess the city! You have 12 hours to reply and win. Quiz nr. "+str(iteration))
    # api.update_with_media('image.png',"A random piece of earth. "+cityline[0]+" Lat: "+str(lat)+" Lon: "+str(lon))
     time.sleep(sleeping)
+
     api.update_status("Nobody has guessed yet... so let's make it easier. Quiz nr. "+str(iteration))
-    zoom = 10
+    zoom = 13
     test = "https://maps.googleapis.com/maps/api/staticmap?center="+str(lat)+","+str(lon)+"&zoom="+str(zoom)+"&size=640x640&maptype=satellite"
     urllib.urlretrieve(test, "image"+".png") #Saves the image overwriting the existing one
     time.sleep(5) #gives time to fetch the content and save it
     api.update_with_media('image.png',"Here it comes a wider view. You have 6 hours left to guess... Quiz nr. "+str(iteration))
     time.sleep(sleeping)
+
+    
     tweets = api.home_timeline()
     print len(tweets)
     print type(tweets)
-    t = [cityline[0]]
+    t = cities.records[n]['name']
     print t
     for s in tweets:
         for i in t:
